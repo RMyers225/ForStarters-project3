e23e0b703f4a8375bfbeded46f2823bec540feeb
RMyers225 <62179690+RMyers225@users.noreply.github.com>
2020-05-22 16:04:37 -0400

    update

--- client/src/components/SinglePlace.js
+++ client/src/components/SinglePlace.js
@@ -6,10 +6,11 @@ export default class SinglePlace extends Component {
 
     state = {
         onePlace: {
-        name: '',
-        address: '',
-        cityState:'',
-        zipCode: Number,
+        Name: '',
+        Address: '',
+        City:'',
+        State:'',
+        ZipCode: 0,
         }
     }
 
@@ -28,27 +29,32 @@ export default class SinglePlace extends Component {
         console.log('placeId', placeId)
         const res = await axios.get(`/api/place/${placeId}`)
         const newState = {...this.state}
+        newState.onePlace = res.data
 
         this.setState(newState)
     }
 
-    onSubmit = async (evt) => {
-        evt.preventDefault()
-        try {
-            await axios.post('/api/place', this.state.onePlace)
-            this.getAllPlaces()
-        } catch (error) {
-            console.log('Failed to create place')
-            console.log(error)
-        }
-    }
+    // onSubmit = async (evt) => {
+    //     evt.preventDefault()
+    //     try {
+    //         await axios.post('/api/place', this.state.onePlace)
+    //         this.getAllPlaces()
+    //     } catch (error) {
+    //         console.log('Failed to create place')
+    //         console.log(error)
+    //     }
+    // }
 
     render() {
 
         return (<div>
             <div>
                 <h1>Single Place</h1>
-            
+                <div>{this.state.onePlace.Name}</div>
+                <div>{this.state.onePlace.Address}</div>
+                <div>{this.state.onePlace.City}</div>
+                <div>{this.state.onePlace.State}</div>
+                <div>{this.state.onePlace.ZipCode}</div>
             </div>
            
             </div>    
