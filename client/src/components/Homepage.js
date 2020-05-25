7b35a1a67ef25a5d0c5fadbebdc66af393fb3968
RMyers225 <62179690+RMyers225@users.noreply.github.com>
2020-05-25 16:02:51 -0400

    single question renders on question component

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -30,7 +30,7 @@ export default class Homepage extends Component {
     componentDidMount() {
         axios.get('/api/homepage')
             .then((res) => {
-                this.setState({message: res.data})
+                this.setState({ message: res.data })
             })
     }
 
@@ -48,6 +48,25 @@ export default class Homepage extends Component {
                 <h1>{this.state.questions}</h1>
                 <h1>{this.state.datePlaces}</h1>
                 <h1>{this.state.events}</h1>
+
+                <p>Whether it's been awkward, boring, or nonexistent the dating world could use some help in the conversation department.<br></br>
+<br></br>
+
+For Starters...is a conversation starter app specifically designed to put some depth and spice back into getting to know each other. It will serve as a question bank of fun, funny, hypothetical and real life questions that are formed to allow you to see someone’s character, not rehearsed “date answer”.<br></br>
+
+<br></br>
+Users can use this as a tool to select a few go-to questions for later, or open and play as a game with your date. Whichever method you choose, you’ll be prepared.<br></br>
+
+<br></br>
+Mothers always say, “It’s not what you ask, it’s how you ask.” So tell me, what’s your starter?</p>
+
+
+                <ul>
+                    <li><a href="default.asp">Home</a></li>
+                    <li><a href="questions.asp">Questions</a></li>
+                    <li><a href="datePlaces.asp">Date Places</a></li>
+                    <li><a href="events.asp">Events</a></li>
+                </ul>
             </div>
         )
     }
--- client/src/components/Questions.js
+++ client/src/components/Questions.js
@@ -2,16 +2,18 @@ import React from 'react'
 // import { Link } from 'react-router-dom'
 import axios from 'axios'
 
-function shuffleQuestion(array) {
-    let i = array.length - 1;
-    for (; i > 0; i--) { 
-        const j = Math.floor(Math.random() * (i + 1));    
-        const temp = array[i]; 
-             array[i] = array[j];   
-         array[j] = temp
-              }
-       return array;
-        } 
+// function shuffleQuestion(array) {
+//     let i = array.length - 1;
+//     for (; i > 0; i--) { 
+//         const j = Math.floor(Math.random() * (i + 1));    
+//         const temp = array[i]; 
+//              array[i] = array[j];   
+//          array[j] = temp
+//               }
+//       const newState = { ...this.state };
+//       newState.allQuestions= array
+//       console.log(newState.allQuestions)
+//         } 
 
 
 
@@ -20,13 +22,27 @@ class Questions extends React.Component {
 
     state = {
         allQuestions: []
-    }
-
-    componentDidMount() {
+    }        
+    
+componentDidMount() {
         this.getAllQuestions()
+}
+
+shuffleQuestion = (array) => {
+    let i = array.length - 1;
+    for (; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        const temp = array[i];
+        array[i] = array[j];
+        array[j] = temp
     }
-
-
+    const newState = { ...this.state };
+    newState.allQuestions = array
+    newState.singleQuestion = array[0].question
+    console.log(newState.singleQuestion)
+    console.log(newState.allQuestions)
+    this.setState(newState)
+} 
 
     getAllQuestions = async () => {
         try {
@@ -34,28 +50,24 @@ class Questions extends React.Component {
             const newState = { ...this.state }
             newState.allQuestions = res.data
             this.setState(newState)
+        this.shuffleQuestion(this.state.allQuestions)
         } catch (error) {
-            console.log('Failed to get all questions')
-            console.log(error)
-        }
+        console.log('Failed to get all questions')
+        console.log(error)
     }
+} 
 
-    render() {
-        const shuffled = shuffleQuestion(this.state.allQuestions);
-        return (
-            <div>
+render() {
+    
+ return (
+        < div >
                 <h1>Question</h1>
-
-                {shuffled.map((question) => {
-                    return (<div key={question._id}>
-                        <div>{question.question}</div>
-
-
-                    </div>
-                    )
-                })}
+    <div>
+        {this.state.singleQuestion}
+    </div>            
+      
                     
-            </div>
+            </div >    
 
         )
     }
