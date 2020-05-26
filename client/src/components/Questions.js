931f16025a4f571d83d1958071135e835739f913
RMyers225 <62179690+RMyers225@users.noreply.github.com>
2020-05-26 17:07:21 -0400

    trying to put a next button on question component

--- client/src/components/Questions.js
+++ client/src/components/Questions.js
@@ -18,64 +18,65 @@ import axios from 'axios'
 
 
 export default 
-class Questions extends React.Component {
-
+class Question extends React.Component {
+    
     state = {
         allQuestions: []
     }          
     
 componentDidMount() {
-        this.getAllQuestions()
-}
-
-shuffleQuestion = (array) => {
-    let i = array.length - 1;
-    for (; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        const temp = array[i];
-        array[i] = array[j];
-        array[j] = temp
-    }
-    const newState = { ...this.state };
-    newState.allQuestions = array
-    newState.singleQuestion = array[0].question
-    console.log(newState.singleQuestion)
-    console.log(newState.allQuestions)
-    this.setState(newState)
-} 
-
+            this.getAllQuestions()
+        
+        
+fleQuestion = (array) => {
+            let i = array.length - 1;
+        for (; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            const temp = array[i];
+            array[i] = array[j];
+            array[j] = temp
+        } 
+    constnewState = { ...this.state };
+        newState.allQuestions = array
+        newState.singleQuestion = array[0].question
+        console.log(newState.singleQuestion)
+        console.log(newState.allQuestions)
+        this.setState(newState)
+        
+    
     getAllQuestions = async () => {
         try {
             const res = await axios.get('/api/question')
-            const newState = { ...this.state }
-            newState.allQuestions = res.data
+        const newState = { ...this.state }
+        newState.allQuestions = res.data
             this.setState(newState)
-        this.shuffleQuestion(this.state.allQuestions)
-        } catch (error) {
-        console.log('Failed to get all questions')
+            this.shuffleQuestion(this.state.allQuestions)
+            tch (error) {
+            ole.log('Failed to get all questions')
         console.log(error)
-    }
-} 
-
-onChangeQuestion = async (questionId) => {
-    await axios.delete(`/api/question/${this.state.allQuestions._id}`, this.state.allQuestions)
-    this.getAllQuestions()
+            
+            
+        
+    oChangeQuestion = async (questionId) => {
+    await axios.get(`/api/question/${this.state.allQuestions._id}`, this.state.allQuestions)
+        this.getAllQuestin()
 }
 
 render() {
     
  return (
-        < div >
-                <h1>Question</h1>
-    <div>
+    < div >
+               <h1>Question</h1>
+       <div>
         {this.state.singleQuestion}
-    </div>                
-        
-    
+            iv>                     
+                
+            
 
-                    
+
+                
             </div >     
 
-        )
+        
     }
 }
